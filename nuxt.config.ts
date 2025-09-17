ea5b0af9237ea694b9caf7aefb2946201c1c2ce8
Cyrille DE GASQUET <pro.cyrille.degasquet@gmail.com>
2025-09-17 04:52:58 +0200

    save

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 export default defineNuxtConfig({
-	title: "Magasin de plantes",
-	compatibilityDate: "2025-07-15",
-	modules: ["nuxt-auth-utils"],
-	devtools: { enabled: true },
-	css: ["~/assets/css/application.css"],
-	app: {
-		head: {
-			link: [{ rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" }],
-			script: [{ src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", tagPosition: "bodyClose" }],
-		},
-	},
+								title: "Magasin de plantes",
+								compatibilityDate: "2025-07-15",
+								modules: ["nuxt-auth-utils", "@nuxt/eslint"],
+								devtools: { enabled: true },
+								css: ["~/assets/css/application.css"],
+								app: {
+																head: {
+																								link: [{ rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" }],
+																								script: [{ src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", tagPosition: "bodyClose" }],
+																},
+								},
 });
