f9e2b666b6ea8a49f409c91cb8eb80811590f9b2
Cyrille DE GASQUET <pro.cyrille.degasquet@gmail.com>
2025-10-02 01:14:30 +0200

    Redirection des pages not-found vers home

---
+++ error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { navigateTo } from '#imports'
+
+const error = useError()
+
+if (error.value?.statusCode === 404) {
+  navigateTo('/plants')
+}
+</script>
+
+<template>
+  <div class="container mt-4 text-center">
+    <p>Redirection vers la page des plantes...</p>
+  </div>
+</template>
